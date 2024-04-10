73de15e0a82b3cc7b28563d12eb731bb8307c500
Jishan Hossain <imranislamjishan200@gmail.com>
2024-04-10 15:44:51 +0600

    text to speatch add

--- app/src/main/java/com/example/testapp/TextToVoiceActivity.java
+++ app/src/main/java/com/example/testapp/TextToVoiceActivity.java
@@ -3,12 +3,40 @@ package com.example.testapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TextToVoiceActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
+    TextToSpeech textToSpeech;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_to_voice);
+
+        textView = findViewById(R.id.text);
+        button = findViewById(R.id.plaBtn);
+
+
+        textToSpeech = new TextToSpeech(TextToVoiceActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = textView.getText().toString();
+                textToSpeech.speak(text , TextToSpeech.QUEUE_FLUSH , null, null);
+            }
+        });
+
     }
 }
