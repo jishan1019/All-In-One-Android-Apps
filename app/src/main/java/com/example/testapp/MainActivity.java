0cee84f54c2f8973ed9a9119467e75020e5917ab
Jishan Hossain <imranislamjishan200@gmail.com>
2024-04-10 16:08:10 +0600

    exit alear add

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -28,6 +30,31 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,AlertDialogActivity.class));
             }
         });
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Are you sure")
+                .setMessage("You want to exit from apps.")
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        finishAndRemoveTask();
+                    }
+                })
+                .show();
 
     }
 }
