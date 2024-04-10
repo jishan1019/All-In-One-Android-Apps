39303522a45143fbbd78358bc3a0d3ec91a4935e
Jishan Hossain <imranislamjishan200@gmail.com>
2024-04-10 15:20:00 +0600

    inital project setup

---
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.textToVoice).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,TextToVoiceActivity.class));
+            }
+        });
+
+    }
+}
